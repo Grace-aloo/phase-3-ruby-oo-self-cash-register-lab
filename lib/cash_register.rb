a4c1d93c46cd50c097a2b02e97d53dbc5dab3bd8
grace_aloo <graceaaloo@gmail.com>
2023-02-13 12:51:59 +0300

    one error left

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,34 +1,38 @@
 class CashRegister
-    attr_accessor :total
-    
+    attr_accessor :total, :discount, :items, :prices
+  
     def initialize(discount=0)
       @total = 0
       @discount = discount
       @items = []
+      @prices = []
     end
-    
+  
     def add_item(title, price, quantity=1)
-      @total += price * quantity
-      quantity.times { @items << [title, price] }
+      self.total += price * quantity
+      self.prices << price
+      quantity.times { self.items << title }
     end
-    
+  
     def apply_discount
-      if @discount == 0
+      if self.discount == 0
         "There is no discount to apply."
       else
-        @total -= @total * @discount / 100
-        "After the discount, the total comes to $#{@total.round(2)}."
+        discount_as_percent = (100.0 - self.discount.to_f) / 100
+        self.total = (self.total * discount_as_percent).to_i
+        #self.total -= self.total * self.discount / 100.0
+        "After the discount, the total comes to $#{self.total.round(2)}."
       end
     end
-    
-    def items
-      @items.map(&:first)
-    end
-    
-    def void_last_transaction
-      last_item = @items.pop
-      return @total = 0.0 if last_item.nil?
   
-      @total -= last_item[1]
+    
+  
+    def void_last_transaction
+        if @items.empty?
+          self.total = 0.0
+        else
+            last_price = @prices.pop
+            self.total -= last_price.to_f
+        end
     end
   end
